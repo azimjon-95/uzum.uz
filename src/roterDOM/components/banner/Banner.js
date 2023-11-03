469c52839e49441fe4289f30b45b23368211cc43
azimjon-95 <azimjon995m@gmail.com>
2023-11-03 15:14:29 +0500

    Banner

--- src/roterDOM/components/banner/Banner.js
+++ src/roterDOM/components/banner/Banner.js
@@ -1,12 +1,36 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
 import './style.css';
+import { Autoplay, Pagination, Navigation } from 'swiper';
+import img from '../../assets/banner/images.jpg'
 
-const Banner = () => {
+function Banner() {
     return (
-        <div className='BannerContainer'>
-            <h1>Banner</h1>
-        </div>
-    )
-}
+        <>
+            <Swiper
+                spaceBetween={30}
+                centeredSlides={true}
+                autoplay={{
+                    delay: 2500,
+                    disableOnInteraction: false,
+                }}
+                pagination={{
+                    clickable: true,
+                }}
+                navigation={true}
+                modules={[Autoplay, Pagination, Navigation]}
+                className="mySwiper"
+            >
+                <SwiperSlide className='SwiperSlide'>
+                    <img src={img} alt="" />
+                </SwiperSlide>
+                
 
-export default Banner;
+            </Swiper>
+        </>
+    );
+}
+export default Banner
